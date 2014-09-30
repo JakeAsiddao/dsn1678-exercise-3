4ab3b1a01228164176a700704364435b726f77c9
JakeAsiddao <asiddao07@hotmail.ca>
2014-09-30 11:11:57 -0400

    Fixed js

--- info-bubble-starter-code/js/main.js
+++ info-bubble-starter-code/js/main.js
@@ -4,7 +4,6 @@ var sales = $('.sales');
 
 var salesButton = function () {
 var isActive = salesDetails.attr('data-state');
-    
     if (isActive == 'active') {
     salesDetails.attr('data-state','inactive');
     } else {
@@ -13,3 +12,19 @@ var isActive = salesDetails.attr('data-state');
 };
 
 sales.on('click', salesButton);
+
+
+var rightSalesDetails = $('.right-sales__details');
+
+var rightSales = $('.right-sales');
+
+var rightSalesButton = function () {
+var isActive = rightSalesDetails.attr('data-state');
+    if (isActive == 'active') {
+    rightSalesDetails.attr('data-state','inactive');
+    } else {
+    rightSalesDetails.attr('data-state','active');
+    }
+};
+
+rightSales.on('click', rightSalesButton);
